61d8139d6c434164d606bb53e77db106e0d58889
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-10 21:37:27 -0300

    Created method to git clone and code analysis

---
+++ dashboard/project/analyse.py
@@ -0,0 +1,16 @@
+import git
+from code_quality_tools.code_quality_tools import CodeQualityCheck
+
+
+def git_clone(url_git, name=None):
+    if name is None:
+        git.Git().clone(url_git)
+    else:
+        git.Git().clone(url_git, name)
+
+
+def code_analysis(project):
+    check = CodeQualityCheck()
+    git_clone(project.url_git, project.name)
+    analysis_result = check.get_all_errors(project.name)
+    return analysis_result
