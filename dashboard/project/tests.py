9b9d80df3c597a537fc9dda6ff58e18e4b5227ad
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-10 21:36:56 -0300

    Created tests for git clone and code analysis

--- dashboard/project/tests.py
+++ dashboard/project/tests.py
@@ -1,16 +1,29 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+import os
+import shutil
 from django.test import TestCase
+from analyse import git_clone, code_analysis
+from models import Project
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestCodeAnalysis(TestCase):
+    fixtures = ['project.json']
+    project_path = 'code_quality_tools'
+
+    def test_should_be_possible_clone_git_repository(self):
+        git_clone('https://github.com/clybob/code_quality_tools.git')
+        path_exists = os.path.exists(self.project_path)
+        self.assertTrue(path_exists)
+        shutil.rmtree(self.project_path)
+
+    def test_should_be_possible_clone_git_repository_passing_directory(self):
+        git_clone('https://github.com/clybob/code_quality_tools.git', 'test')
+        path_exists = os.path.exists('test')
+        self.assertTrue(path_exists)
+        shutil.rmtree('test')
+
+    def test_should_return_dictionary_with_quality_information(self):
+        project = Project.objects.get(id=1)
+        analysis_result = code_analysis(project)
+        self.assertTrue(type(analysis_result) is dict)
+        self.assertEqual(len(analysis_result), 5)
+        shutil.rmtree(project.name)
