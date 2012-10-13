de038d73eabc57b44e51230fead3debca917d212
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-13 16:27:29 -0300

    Created base tests for details and analyse views

---
+++ dashboard/project/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from dashboard.project.models import Analysis
+
+
+class TestViews(TestCase):
+    fixtures = ['project.json', 'analysis.json']
+
+    def setUp(self):
+        self.analysis = Analysis.objects.get(id=1)
+
+    def test_project_details_should_be_acessible(self):
+        url = '/project/%d/' % self.analysis.project.id
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_absent_project_should_not_be_acessible(self):
+        url = '/project/999/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_project_analyse_should_be_acessible(self):
+        url = '/project/%d/analyse/' % self.analysis.project.id
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_absent_project_analyse_should_not_be_acessible(self):
+        url = '/project/999/analyse/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
