651f3e27f593ee6e03d1bd48f63a533a2a3f6e62
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-13 15:36:25 -0300

    Added url to analyse project

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^project/(?P<project_id>\d+)/$', 'dashboard.project.views.detail'),
+    url(r'^project/(?P<project_id>\d+)/analyse/$', 'dashboard.project.views.analyse'),
     url(r'^admin/', include(admin.site.urls)),
 )
