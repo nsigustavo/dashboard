763cee601fd313bea16648758c4d32895ef0609a
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-13 15:41:47 -0300

    Changed date fields to datetime fields and created method to return date for humans

--- dashboard/project/models.py
+++ dashboard/project/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     url_git = models.CharField(max_length=256, null=False, blank=False)
-    date_created = models.DateField(auto_now_add=True, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
 
     class Meta:
         db_table = 'dashboard_project'
@@ -18,7 +18,7 @@ class Project(models.Model):
 
 class Analysis(models.Model):
     project = models.ForeignKey(Project)
-    date_executed = models.DateField(auto_now_add=True, editable=False)
+    date_executed = models.DateTimeField(auto_now_add=True, editable=False)
     pep8 = models.PositiveIntegerField(default=0)
     pyflakes = models.PositiveIntegerField(default=0)
     clonedigger = models.PositiveIntegerField(default=0)
@@ -29,12 +29,16 @@ class Analysis(models.Model):
     class Meta:
         db_table = 'dashboard_analysis'
 
+    @property
+    def date_executed_for_humans(self):
+        return self.date_executed.strftime("%c")
+
     def __str__(self):
         project_name = self.project.name
-        date_executed = self.date_executed.strftime("%m/%d/%Y")
+        date_executed = self.date_executed_for_humans
         return 'Analysis of project(%s) on %s' % (project_name, date_executed)
 
     def __unicode__(self):
         project_name = self.project.name
-        date_executed = self.date_executed.strftime("%m/%d/%Y")
+        date_executed = self.date_executed_for_humans
         return u'Analysis of project(%s) on %s' % (project_name, date_executed)
