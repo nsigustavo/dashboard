60296522df8a9178814f23abf6be755f133ace00
Romulo Tavares <clybob@hotmail.com>
2012-11-05 13:05:40 -0200

    Changed url to list all projects

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^project/$', 'dashboard.project.views.all_projects'),
+    url(r'^projects/$', 'dashboard.project.views.all_projects'),
+    url(r'^projects/create$', 'dashboard.project.views.create_project'),
     url(r'^project/(?P<project_id>\d+)/$', 'dashboard.project.views.detail'),
     url(r'^project/(?P<project_id>\d+)/analyze/$', 'dashboard.project.views.analyze'),
     url(r'^project/(?P<project_id>\d+)/history/(?P<metric_name>\w+).json', 'dashboard.project.views.history'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^projects/$', 'dashboard.project.views.all_projects'),
-    url(r'^projects/create$', 'dashboard.project.views.create_project'),
 )
 
 handler404 = 'dashboard.views.view_404'
